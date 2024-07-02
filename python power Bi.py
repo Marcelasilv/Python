fe21a072fd16a70a16826b21dd67c43cf88de23b
Marcela Silva <172087060+Marcelasilv@users.noreply.github.com>
2024-07-01 22:29:06 -0300

    Create python power Bi.py

---
+++ python power Bi.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.PAUSE = 0.5
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+pyautogui.write("link")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
